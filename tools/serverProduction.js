1d0950f248b49b54ff75508404a1e2ad339ee223
leonardo a <leonardo.akato@gmail.com>
2018-05-04 08:24:51 -0300

    added cors thing to production server

--- tools/serverProduction.js
+++ tools/serverProduction.js
@@ -9,6 +9,24 @@ const logger = require('./logger');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header("Access-Control-Allow-Headers","*")
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use('/api', router);
 app.use(logger.errorHandler());
 
