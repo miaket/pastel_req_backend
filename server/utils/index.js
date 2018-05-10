c679e0736c8bde430d229ae219e09e4c0a33c3e9
leonardo a <leonardo.akato@gmail.com>
2018-05-10 16:48:51 -0300

    add util index

---
+++ server/utils/index.js
@@ -0,0 +1,9 @@
+const encryptor = require('./encryptor');
+const decryptor = require('./decryptor');
+const validator = require('./validator');
+
+module.exports = {
+  encryptor,
+  decryptor,
+  validator,
+};
