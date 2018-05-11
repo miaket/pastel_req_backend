a366b7151f2b35ce9c43f6e5fe3aaffb5c396e88
leonardo a <leonardo.akato@gmail.com>
2018-05-11 12:35:44 -0300

    add constant msg

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,9 +1,11 @@
 const encryptor = require('./encryptor');
 const decryptor = require('./decryptor');
 const validator = require('./validator');
+const constants = require('./constants');
 
 module.exports = {
   encryptor,
   decryptor,
   validator,
+  constants,
 };
