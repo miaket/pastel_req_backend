cdaa00cf37ac335a120e15399e3ca85015747562
leonardo a <leonardo.akato@gmail.com>
2018-05-24 15:27:46 -0300

    fix get customers API

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -2,19 +2,18 @@ const Customer = require('../models').Customer;
 
 module.exports = {
   create(req, res) {
-    // let encrypPass = encryptor(req.body.password, key);
     return Customer
       .create({
           customerNumber: req.body.customerNumber,
           customerName: req.body.customerName,
         })
         .then(user => res.status(201).send(user))
-        .catch(error => res.status(400).send(error));
+        .catch(error => res.status(400).send(error))
   },
   list(req, res) {
     return Customer
       .findAll()
-      .then(user => res.status(200).send(Customer))
+      .then(Customer => res.status(200).send(Customer))
       .catch(error => res.status(400).send(error));
   }
 };
