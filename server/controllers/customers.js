0dd3d7085ddcaf937c80fc7eed17f046af315caa
leonardo a <leonardo.akato@gmail.com>
2018-06-05 17:56:08 -0300

    bolicia

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -7,8 +7,8 @@ module.exports = {
         regNumber: req.body.regNumber,
         name: req.body.name,
       })
-        .then(user => res.status(201).send(user))
-        .catch(error => res.status(400).send(error))
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error))
   },
   list(req, res) {
     return Customer
