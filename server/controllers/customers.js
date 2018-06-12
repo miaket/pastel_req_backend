847c7ee450707be086aad56bab4bf3a605ba490e
leonardo a <leonardo.akato@gmail.com>
2018-06-12 16:32:15 -0300

    create customer validator API

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,19 +1,48 @@
 const Customer = require('../models').Customer;
 
+const getCustomer = function(regNumberId){
+  return Customer
+    .findOne({
+      where: {regNumber: regNumberId},
+      attributes: [
+        'id',
+        'regNumber',
+        'name'
+      ],
+    })
+      .then(Customer => Customer)
+      .catch(error => error);
+}
+
+const createCustomer = function(req) {
+  return Customer
+    .create({
+      regNumber: req.body.regNumber,
+      name: req.body.name,
+    })
+    .then(customer => {
+      return customer;
+    })
+    .catch(error => res.status(400).send(error))
+}
+
 module.exports = {
-  create(req, res) {
-    return Customer
-      .create({
-        regNumber: req.body.regNumber,
-        name: req.body.name,
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error))
-  },
   list(req, res) {
     return Customer
       .findAll()
       .then(Customer => res.status(200).send(Customer))
       .catch(error => res.status(400).send(error));
+  },
+  validCustomer(req, res){
+    const { regNumber, name } = req.body;
+    return getCustomer(regNumber).then(Customer => {
+      if (Customer){
+        return res.status(200).send(Customer);
+      } else{
+        return createCustomer(req)
+          .then(Customer => res.status(400).send(Customer))
+          .catch(error => res.status(400).send(error))
+      }
+    })
   }
 };
