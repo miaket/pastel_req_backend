3a38866806e2578eb2902fc0f237b19acb80b82b
leonardo a <leonardo.akato@gmail.com>
2018-06-14 12:13:40 -0300

    geworking t arrayay of customers API - call a findOrCreate

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,6 +1,8 @@
 const Customer = require('../models').Customer;
 
 const getCustomer = function(regNumberId){
+  console.log('local get cust')
+  console.log(regNumberId)
   return Customer
     .findOne({
       where: {regNumber: regNumberId},
@@ -10,19 +12,32 @@ const getCustomer = function(regNumberId){
       .catch(error => error);
 }
 
+
 const createCustomer = function(req) {
   const { regNumber, name } = req.body
   return Customer
-    .create({ regNumber, name})
-    .then(customer => {
-      return customer;
-    })
-    .catch(error => res.status(400).send(error))
+  .create({ regNumber, name})
+  .then(customer => {
+    return customer;
+  })
+  .catch(error => res.status(400).send(error))
 }
 
 module.exports = {
-  getCustomer(req, res) {
-    const { regNumber } = req.params;
+  getArrayCustomer(req, res){
+    regNumbers = req.body.regNumber
+    console.log(regNumbers)
+    return Customer.findAll({ where: { regNumber: regNumbers } })
+      .then(customers => {
+        console.log(customers)
+        res.status(201).send(customers)
+      })
+    .catch(error => res.status(400).send(error));
+  },
+  getCustomer(regNumber) {
+    console.log('inside getcustomer')
+    console.log(req.body)
+    //const { regNumber } = req.body;
     return getCustomer(regNumber).then(Customer => {
       if (Customer){
         return res.status(200).send(Customer);
@@ -38,13 +53,24 @@ module.exports = {
     console.log(req.body)
     const { regNumber, name } = req.body;
     return getCustomer(regNumber).then(Customer => {
+      console.log(Customer);
       if (Customer){
         return (Customer);
-      } else{
+      } else {
         return createCustomer(req)
           .then(Customer => (Customer))
           .catch(error => (error))
       }
     })
+  },
+  findOrCreate(req){
+    const { regNumber, name} = req.body
+    return Customer
+      .findOrCreate({where: {regNumber: regNumber}, defaults: {name: name}})
+      // .spread((Customer, created) => {
+      //   Customer.get({plain: true})}
+      // )
+      .then(Customer => res.status(200).send(Customer))
+      .catch(error => res.status(400).send(error))
   }
 };
