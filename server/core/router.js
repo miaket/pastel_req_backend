aaf4e657bff5ac275967f549ad1444cd929890be
leonardo a <leonardo.akato@gmail.com>
2018-06-14 12:14:24 -0300

    route find array of customers API

--- server/core/router.js
+++ server/core/router.js
@@ -26,9 +26,11 @@ router.get('/user/all', usersController.list);
 router.post('/customer/validatecreate', customersController.validCustomer);
 router.get('/customer/:regNumber', customersController.getCustomer);
 
+router.post('/findcreate', customersController.findOrCreate);
 router.post('/req/:userId/reqcreate', requestsController.create);
 router.post('/customerlink', requestsController.customerlink);
 // router.put('/req/customer/:id', requestsController.RequestCustomer);
+router.post('/getarraycustomers', customersController.getArrayCustomer);
 router.get('/req/myrequests/:userId', myRequestsController.myRequests);
 
 // router.post('/requestcustomer/create', requestcustomersController.create);
