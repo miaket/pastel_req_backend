9bdb380be02266ed03b9774f744474d4c6994e39
leonardo a <leonardo.akato@gmail.com>
2018-06-20 14:05:00 -0300

    working new Request API binding customer by sending regNumbers array - FINALLY

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -2,17 +2,35 @@ const User = require('../models').User;
 const Request = require('../models').Request;
 const Customerinfo = require('../models').Customerinfo;
 const Customer = require('../models').Customer;
+const controlCustomers = require('./customers');
 
+//getArrayCustomer <- send array of customer's regNumbers
 module.exports = {
   create(req, res) {
-    const { message, urgencyLevel, complete, customersId } = req.body
+    const { message, urgencyLevel, complete, regNumber } = req.body
     const { userId } = req.params
-    return Request
-      .create({ message, urgencyLevel, complete, userId })
-        .then(newRequest => newRequest.addCustomers(customersId))
-          .then(user => res.status(200).send(user))
-          .catch(error => res.status(400).send(error))
-        .catch(error => res.status(400).send(error))
+    controlCustomers.getArrayCustomer(regNumber)
+      .then(regNumbers =>{
+        return Request
+          .create({ message, urgencyLevel, complete, userId })
+            .then(newRequest => newRequest.addCustomers(regNumbers))
+              .then(user => res.status(200).send(user))
+              .catch(error => res.status(400).send(error))
+            .catch(error => res.status(400).send(error))
+        .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  customerlink(req,res){
+    console.log('dear ol dad')
+    console.log (req.body)
+    return controlCustomers.validCustomer(req)
+      .then(Customer => {
+        console.log ('michel , vai toma noseu cu')
+        console.log(Customer)
+        return res.status(200).send(Customer)
+      })
+      .catch(error => res.status(400).send(error));
   },
   // RequestCustomer(req, res) {
   //   return Request.findOne({ where: { id: req.params.id } })
